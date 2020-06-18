308436f38e0dc525b1663eb663e6fe8dc868576d
AlexandreSiriusSchool <alexandre@siriusschool.be>
2020-06-18 17:34:59 +0200

    ExerciceJavaScriptVariable(Done)

---
+++ Exercices/exercices-javascript/les-variables/script.js
@@ -0,0 +1,46 @@
+const text001 = 'Hello World';
+let intro = "Bonjour je m'appelle";
+const firstName = 'Alexandre';
+console.log(intro + ' ' + firstName);
+let introAge = "j'ai";
+let ageNumber = '21';
+const age = 'ans';
+console.log(
+  intro + ' ' + firstName + ',' + introAge + ' ' + ageNumber + ' ' + age + '.'
+);
+let studentTeamAArray = [
+  'Madja',
+  'Fanny',
+  'Jean Eudes',
+  'Nathalie',
+  'Lionel',
+  'Jason',
+  'Regine',
+  'Mohamed',
+  'Alexandre',
+  'Jeremy',
+  'Yana',
+  'Gilles',
+  'Antoine',
+  'Ziadoon',
+  'Fatima',
+  'Ezéchiel',
+  'Marie',
+  'Thierry',
+  'Edouard',
+  'Méthode',
+  'Sarra',
+  'Jeremie',
+  'Joéllie',
+  'Abdoulaye',
+  'Jean Paul',
+  'Natascha',
+];
+console.log(studentTeamAArray[6]);
+console.log(studentTeamAArray[8]);
+const student = {
+  name: 'Maes',
+  firstname: 'Alexandre',
+  age: '21',
+};
+console.log(student.firstname);
