153a61cc21b70fea7302b85bc4d23b2c1c674080
AlexandreSiriusSchool <alexandre@siriusschool.be>
2020-06-18 20:09:51 +0200

    JS. ex: 2.8 & 2.9 DizzynessOnClick (in progress)

--- Exercices/exercices-javascript/les-opérations/script.js
+++ Exercices/exercices-javascript/les-opérations/script.js
@@ -1,26 +1,73 @@
 //Insérez ici les exercices 2.1 à 2.4
 
+function root(a) {
+  return a * a;
+}
+console.log(root(2));
+console.log(root(4));
+console.log(root(8));
+console.log(root(16));
+
+function modulo(a) {
+  return a % 2;
+}
+console.log(modulo(3));
+console.log(modulo(178));
+console.log(modulo(65));
+console.log(modulo(98));
+console.log(modulo(123));
+function percentOff(a, b) {
+  return (a * b) / 100;
+}
+console.log(percentOff(79.99, 19));
+
 // Insérez ici les exercices 2.4 à 2.8 en utilisant les variables déjà créées à votre disposition
 
-let firstNumber = 9;
-let secondNumber = 8;
-let thirdNumber = 4;
-let fourthNumber = 12;
+let firstNumber = 3; // Anciennement 9
+let secondNumber = 11; // Anciennement 8
+let thirdNumber = 12; // Anciennement 4
+let fourthNumber = 9; // Anciennement 12
+
+function dividePerThree(a) {
+  return a / 3;
+}
+console.log(dividePerThree(firstNumber));
+
+function add(a, b) {
+  return a + b;
+}
+console.log(add(firstNumber, secondNumber));
+
+function multiply(a, b) {
+  return a * b;
+}
+console.log(multiply(firstNumber, thirdNumber));
+
+function substract(a, b) {
+  return a - b;
+}
+
+console.log(substract(fourthNumber, firstNumber));
 
 // Exercice incrémentation
 function incrementOnClick() {
-    document.getElementById("increment").addEventListener("click", function decrement() {
-        // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
-        let incrementCounter = 0;
-
-       console.log("Incrémentez") 
-})};
+  document
+    .getElementById('increment')
+    .addEventListener('click', function decrement(decrementCounter) {
+      console.log(decrementCounter);
+      // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
+      let incrementCounter = 0;
+      console.log('Incrémentez');
+    });
+}
 // Exercice incrémentation
 function decrementOnClick() {
-    document.getElementById("decrement").addEventListener("click", function increment() {
-        // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
-        let decrementCounter = 100;
-
-       console.log("Décrémentez");
-})};
-
+  document
+    .getElementById('decrement')
+    .addEventListener('click', function increment(incrementCounter) {
+      console.log(incrementCounter);
+      // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
+      let decrementCounter = 100;
+      console.log('Décrémentez');
+    });
+}
